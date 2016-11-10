2d37e79422cada3fc99aac42309f119fc701ceaf
zachary_diemer <zdiemer21@gmail.com>
2016-11-09 21:43:24 -0500

    Made edits to SpaceXDriver class - override some java applet methods - and Add other config files

--- src/game/SpaceXDriver.java
+++ src/game/SpaceXDriver.java
@@ -6,4 +6,23 @@ import java.awt.event.*;
 
 public class SpaceXDriver extends Applet {
 	
+	/**
+	 * Overriding the specific functions init, paint, and update from Applet Class.
+	 * We are overriding init to get our game started; we are overriding paint to make the applet draw our games graphics;
+	 * we are overriding update to prevent the applet window from flickering 
+	 */
+	
+	public void init(){
+		resize(500,500); // make sure the applet is the right size
+	}
+	
+	public void paint(Graphics gfx){
+		 gfx.setColor(Color.red);
+		 gfx.fillOval(0,0,50,50); // draw a red circle
+	}
+	
+	public void update(Graphics gfx){
+		 paint(gfx); // call paint without clearing the screen
+	} 
+
 }
